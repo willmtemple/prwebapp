1ddd5ab65035e7f9f062236f3b0b1435aa51e828
William Temple <William.Temple@colorado.edu>
2018-04-28 18:12:13 -0600

    Layout changes
    Signed-off-by: William Temple <William.Temple@colorado.edu>

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,5 +2,7 @@
 
 $('.ui.radio.checkbox').checkbox();
 
-var mdc = new showdown.Converter();
+var mdc = new showdown.Converter({
+	headerLevelStart : 2
+});
 $("div.mdfmt").html(function(_, t) { return mdc.makeHtml(t); });
